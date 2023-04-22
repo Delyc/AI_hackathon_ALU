292f1106a9f52abf6d60ffaff7ddc7b61365f8eb
Mum Kiir <m.kiir@alustudent.com>
2023-04-22 20:24:59 +0200

    new commit

--- data/steps.js
+++ data/steps.js
@@ -3,12 +3,12 @@ export const services = [
     {
 
         step: "Free call/chat",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dignissimos aspernatur nam consequuntur provident facilis similique. Fuga commodi culpa ex, ducimus suscipit veniam ipsa placeat eos amet numquam consequatur modi.",
+        description: "Our platform enables you to interact with our chatbot using both voice calls and text-based chat. To connect with our chatbot, please call this number: +18332725779.",
         image : "/assets/chat.png"
     },
     {
         step: "Compile your information",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dignissimos aspernatur nam consequuntur provident facilis similique. Fuga commodi culpa ex, ducimus suscipit veniam ipsa placeat eos amet numquam consequatur modi.",
+        description: "Our platform covers key financial topics and questions that you might have, such as retirement planning, debt management, or investment strategies.",
         image : "/assets/plan.png"
     },
     
@@ -20,7 +20,7 @@ export const services2 = [
     {
 
         step: "Create a clear plan",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dignissimos aspernatur nam consequuntur provident facilis similique. Fuga commodi culpa ex, ducimus suscipit veniam ipsa placeat eos amet numquam consequatur modi.",
+        description: "Our platform helps you achieve financial independence for you and your family. With our tools and resources, you can create a clear financial plan to reach your goals. Let us guide you towards a brighter financial future.",
         image : "/assets/chat.png"
     },
 
